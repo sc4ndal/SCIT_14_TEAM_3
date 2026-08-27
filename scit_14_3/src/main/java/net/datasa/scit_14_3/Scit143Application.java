902f2e766eb16cab752a075dc9229cae42c9d9e9
HaruAki <100898033+sc4ndal@users.noreply.github.com>
2026-08-27 16:21:44 +0900

    Update Scit143Application.java

--- scit_14_3/src/main/java/net/datasa/scit_14_3/Scit143Application.java
+++ scit_14_3/src/main/java/net/datasa/scit_14_3/Scit143Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Scit143Application {
-11111
+
 	public static void main(String[] args) {
 		SpringApplication.run(Scit143Application.class, args);
 	}
